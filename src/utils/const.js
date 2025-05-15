e2f52b2e91796b2b9d0e38e31e33b0aa2de00eee
Sparsh Singh <sparshsingh03012003@gmail.com>
2025-05-16 04:24:17 +0530

    deploy

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,8 +1,8 @@
 export const BASE_URL =
   location.hostname === "localhost"
     ? "http://localhost:7777"
-    : "lovenest-production.up.railway.app";
-  
+    : "lovenest-production.up.railway.app"; 
+
 import io from "socket.io-client";
 
 export const createSocketConnections = () => {
